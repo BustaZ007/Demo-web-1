d9b6c27d799bdedfcb471463b5c22d6b62c9d3aa
Dementor911 <lhfujy7477@gmail.com>
2019-09-12 04:50:58 +0500

    adding posts

--- src/main/java/com/example/demoweb/model/PostService.java
+++ src/main/java/com/example/demoweb/model/PostService.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class PostService {
     public Post[] listAllPosts(){
-        return new Post[]{new Post("First post", 11),
+        return new Post[]{new Post("First post", 0),
 
-                new Post("second post", 150),
+                new Post("second post", 1),
 
-                new Post("Third post.", 1000)};
+                new Post("Third post.", 2)};
     }
 }
